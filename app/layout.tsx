5777194eb55cdbc0ad189da859509f5e2662b9b6
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-01 08:21:45 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import {ContextProvider} from './context'
@@ -17,7 +18,30 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body className={inter.className}>
-        <h1>Was ist ein Tarifvertrag ?</h1>
+        <header style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0.2rem',
+        }}>
+        <h1 style={{
+          padding: '1rem',
+        }}>Was ist ein Tarifvertrag ?</h1>
+
+        <nav>
+        <Link href={'/'}><p style={{
+          backgroundColor: 'rgb(228, 230, 173)',
+          borderRadius: '1rem',
+          padding: '1rem',
+        }}>Home</p></Link>
+        <Link href={'/more'}><p style={{
+          backgroundColor: 'rgb(228, 230, 173)',
+          borderRadius: '1rem',
+          padding: '1rem',
+        }}>Mehr Wissen</p></Link>
+        </nav>
+        
+        </header>
+
        
 <ContextProvider>
 {children}
---
+++ app/more/page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function More() {
+  return (
+    <section style={{
+        padding: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+
+    }}>
+        <div style={{
+            margin: '1rem 0',
+            alignSelf: 'center',
+        }}>
+        <h2 style={{
+            color: 'rgb(94, 94, 3)',
+        }}>Mehr Wissen</h2>
+<p>über Tarifverträge</p>
+        </div>
+
+<h3>Externe Links zu</h3>
+<div style={{
+    border: '0.4rem solid rgb(228, 230, 173)',
+    borderRadius: '1rem',
+    padding: '0.2rem',
+
+}}>
+<Link href={'https://www.igmetall.de/tarif/tariflexikon/tarifvertrag2'}>
+<p style={{
+          backgroundColor: 'rgb(228, 230, 173)',
+          borderRadius: '1rem',
+          padding: '1rem',
+  
+        }}>Tarif Lexikon
+            </p></Link><p> der IG Metal Gewerkschaft</p>
+</div>
+
+    </section>
+  )
+}
