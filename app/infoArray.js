95ca77322e4952c0b10251739d41eca5b443d66b
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-05 07:33:11 +0100

    Add files via upload

---
+++ app/infoArray.js
@@ -0,0 +1,24 @@
+export const infoArray = [
+
+    {
+        name: 'Tarifvertrag einfach erklärt (explainity® Erklärvideo) - Hans-Böckler-Stiftung',
+        from: 'explainity GmbH',
+        questions: ['Was ist ein Tarifvertrag ?','Wo steht ein Tarifvertrag in der gesetzlichen Hyrachie ?'],
+        youtube: '4-1OkIDJ_Js?si=2utoIqRT-QCxycUJ',
+        link: ''
+    },
+    {
+        name: 'Tarifverhandlung einfach erklärt (explainity® Erklärvideo) - Hans-Böckler-Stiftung',
+        from: 'explainity GmbH',
+        questions: ['Was ist ein Tarifvertrag ?','Wo steht ein Tarifvertrag in der gesetzlichen Hyrachie ?','Wie läuft ein Tarifverhandlung ab ?'],
+        youtube: '_f-ZtW7AFuM?si=uYLS6_izR0aFihq2',
+        link: ''
+    },
+    {
+        name: 'Günstigkeitsprinzip Tarifvertrag',
+        from: 'Harald Lexis',
+        questions: ['Was ist ein Tarifvertrag ?','Wo steht ein Tarifvertrag in der gesetzlichen Hyrachie ?'],
+        youtube: 'etJYNKImclo?si=lKTbhTdOrA722AxT',
+        link: ''
+    }
+]
