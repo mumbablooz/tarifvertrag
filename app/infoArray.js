fbbb18bf2d484dea728da7994ce513d9eaec067e
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-05 08:02:21 +0100

    Aktualisieren von infoArray.js

--- app/infoArray.js
+++ app/infoArray.js
@@ -1,6 +1,6 @@
 export const infoArray = [
 
-    {
+   /* {
         name: 'Tarifvertrag einfach erklärt (explainity® Erklärvideo) - Hans-Böckler-Stiftung',
         from: 'explainity GmbH',
         questions: ['Was ist ein Tarifvertrag ?','Wo steht ein Tarifvertrag in der gesetzlichen Hyrachie ?'],
@@ -13,7 +13,7 @@ export const infoArray = [
         questions: ['Was ist ein Tarifvertrag ?','Wo steht ein Tarifvertrag in der gesetzlichen Hyrachie ?','Wie läuft ein Tarifverhandlung ab ?'],
         youtube: '_f-ZtW7AFuM?si=uYLS6_izR0aFihq2',
         link: ''
-    },
+    },*/
     {
         name: 'Günstigkeitsprinzip Tarifvertrag',
         from: 'Harald Lexis',
